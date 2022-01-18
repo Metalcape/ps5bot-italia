b1653931d6b40fc848c97d51f04d658b115f6aba
Metalcape <dario.capecchi.m@gmail.com>
2022-01-18 18:00:56 +0100

    generate config

---
+++ generate_config.py
@@ -0,0 +1,18 @@
+import json
+
+credentials = {
+    "amazon" : {
+        "email" : "user@domain.com",
+        "pass" : "password"
+    },
+    "credit_card" : {
+        "number" : "1234567890",
+        "owner" : "Mario Rossi",
+        "month" : "1",
+        "year" : "22",
+        "cvv" : "333"
+    }
+}
+
+with open("config.json", 'w') as outfile:
+    json.dump(credentials, outfile, indent=4, sort_keys=False)
